360e1924a218af46cdb4b8e2300edee8ac13c194
Athul Muralidharan <athulmurali@gmail.com>
2018-06-24 19:18:23 -0400

    Added navigation to trueFalseQuestion

--- container/QuestionContainer/TrueFalseContainer/QuestionTrueFalseContainer.js
+++ container/QuestionContainer/TrueFalseContainer/QuestionTrueFalseContainer.js
@@ -1,11 +1,19 @@
 import React from "react";
 import {Text} from "react-native-elements";
-import {View} from "react-native";
+import {TextInput, View} from "react-native";
+import {essayTextBox, questionEditBarStyle} from "../../../styles/essayQuestionWidget";
+import QuestionHeader from "../../../elements/QuestionHeader";
+import AnswerContainer from "../../../components/AnswerContainer";
+import QuestionParagraph from "../../../elements/QuestionParagraph";
+import QuestionNavigationBar from "../../QuestionNavigationBar";
+import QuestionEditBar from "../../QuestionEditBar";
+import {questionNavigationBarStyle} from "../../../styles/QuestionCommonStyle";
 
 export default class QuestionTrueFalseContainer extends React.Component{
     static navigationOptions={
         title : "True False Question"
     }
+
     constructor(props){
         super(props)
 
@@ -14,15 +22,35 @@ export default class QuestionTrueFalseContainer extends React.Component{
     componentDidMount(){
         console.log("QuestionTrueFalseContainer : Mounted");
     }
+    componentWillUnmount(){
+        console.log("QuestionTrueFalseContainer : Unmounted");
+    }
+
+
+
+
 
 
     render(){
+        const questionText = "Longest paragraph in the world is not really easy to type. " +
+            "That's why I keep the descriptions short"
+        const questionIndex = 1
+        const questionPoints = 100
+
+        return <View
+            style ={{padding : 14}}>
+            <QuestionHeader questionIndex ={questionIndex}   questionName={"sasas"} points ={questionPoints}/>
+            <QuestionParagraph questionText = {questionText}  style={{padding :20}}/>
+            <AnswerContainer style={{padding :20}}>
 
 
-        return <View>
-            <Text>True or False</Text>
+
+            </AnswerContainer>
+            <QuestionNavigationBar
+                style={questionNavigationBarStyle}/>
+            <QuestionEditBar
+                style ={questionEditBarStyle}/>
+
         </View>
-
-
     }
 }
---
+++ styles/QuestionCommonStyle.js
@@ -0,0 +1,3 @@
+export const questionNavigationBarStyle =
+{flexDirection: "row", justifyContent: 'space-between', alignItems: 'center'}
+
